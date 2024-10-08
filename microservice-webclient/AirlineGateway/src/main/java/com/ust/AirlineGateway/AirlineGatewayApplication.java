2e9c5212da938b5300ade32c55b4122e703cd8cd
Sai Venkat <venkatkarnati22@gmail.com>
2024-10-08 19:10:59 +0530

    project

---
+++ microservice-webclient/AirlineGateway/src/main/java/com/ust/AirlineGateway/AirlineGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.ust.AirlineGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AirlineGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineGatewayApplication.class, args);
+	}
+
+}
